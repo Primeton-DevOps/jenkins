49eba857eed4deec5dd3a5bf01c455d1d56deedc
ZhongWen Li <lizhongwen1989@gmail.com>
2016-11-18 10:55:07 +0800

    Update

--- dff/src/test/java/com/primeton/devops/test/ResourceQuotaCreateByJenkinsPipelineTestCase.java
+++ dff/src/test/java/com/primeton/devops/test/ResourceQuotaCreateByJenkinsPipelineTestCase.java
@@ -21,11 +21,15 @@ import com.primeton.devops.dff.utils.ServiceLoaderUtil;
 import com.primeton.devops.dff.utils.VelocityUtil;
 
 /**
- * JobCreateTestCase.
+ * 
+ * 1) 根据Velocity模版生成jenkins任务配置文件(使用Pipeline类型任务)，jenkins任务配置文件中包含如部署如OpenShift资源的yaml文件内容，可以使用Velocity的include语法；<br>
+ * 2) 调用Jenkins的REST API创建部署资源的任务；<br>
+ * 3) 调用Jenkins的REST API执行部署资源的任务；<br>
+ * 4) 调用Jenkins的REST API获取执行任务的日志；<br>
  *
  * @author ZhongWen Li (mailto:lizw@primeton.com)
  */
-public class JobCreateTestCase extends AbstractTestCase {
+public class ResourceQuotaCreateByJenkinsPipelineTestCase extends AbstractTestCase {
 	
 	private static final String AUTH_TOKEN = "W5TLNgNN6E_FZVaNkyNiBW_0JM2VrOFM3KpZqPR3EEc";
 
@@ -126,12 +130,33 @@ public class JobCreateTestCase extends AbstractTestCase {
 		return yaml.replaceAll("'", "\\\\&apos;");
 	}
 	
+	private String getResourceQuota2(String resourceName) throws FileNotFoundException {
+		String template = getTemplateAsString("classpath:/templates/openshift/ResourceQuota.velocity2.yaml");
+		System.err.println(template);
+		
+		Map<String, Object> context = new HashMap<>();
+		context.put("name", resourceName);
+//		context.put("memory", 100);
+//		context.put("cpu", "200");
+//		context.put("pods", "100");
+//		context.put("services", "100");
+//		context.put("replicationcontrollers", "50");
+//		context.put("resourcequotas", "10");
+		
+		String yaml = VelocityUtil.parse(template, context, "ResourceQuota.yaml");
+		System.out.println();
+		System.out.println(yaml);
+		System.out.println();
+		
+		return yaml.replaceAll("'", "\\\\&apos;");
+	}
+	
 	/**
 	 * 
 	 * @throws FileNotFoundException
 	 * @throws JobException
 	 */
-	private void createDeleteResourceQuotaJob() throws FileNotFoundException, JobException {
+	private void createDeleteResourceQuotaJob(String resourceDescriptor) throws FileNotFoundException, JobException {
 		String template = getTemplateAsString("classpath:/templates/job/DeleteResourcePipelineJobTemplate.velocity.xml");
 		System.err.println(template);
 		
@@ -149,10 +174,11 @@ public class JobCreateTestCase extends AbstractTestCase {
 		
 		openshiftSettings.put("url", "https://192.168.2.91:8443");
 		openshiftSettings.put("authToken", AUTH_TOKEN);
-		openshiftSettings.put("resourceName", resourceName);
-		openshiftSettings.put("resourceType", "ResourceQuota");
+//		openshiftSettings.put("resourceName", resourceName);
+//		openshiftSettings.put("resourceType", "ResourceQuota");
+		openshiftSettings.put("resourceDescriptor", resourceDescriptor);
 		openshiftSettings.put("namespace", namespace);
-		openshiftSettings.put("verbose", "false");
+		openshiftSettings.put("verbose", "true");
 		
 		piplelineSettings.put("sandbox", "true");
 		
@@ -176,24 +202,19 @@ public class JobCreateTestCase extends AbstractTestCase {
 		super.clean();
 		// Delete ResourceQuota
 		try {
-			createDeleteResourceQuotaJob();
-			runDeleteResourceQuotaJobWithClean();
+			createDeleteResourceQuotaJob(getResourceQuota2(resourceName));
+			
+			JobApi api = ServiceLoaderUtil.load(JobApi.class);
+			api.runJob(jobName2);
+			
+			TimeUnit.SECONDS.sleep(90);
+			
+			api.deleteJob(jobName2);
+			api.deleteJob(jobName);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	/**
-	 * 
-	 */
-	private void runDeleteResourceQuotaJobWithClean() throws Exception {
-		JobApi api = ServiceLoaderUtil.load(JobApi.class);
-		api.runJob(jobName2);
-		
-		TimeUnit.SECONDS.sleep(90);
-		
-		api.deleteJob(jobName2);
-		api.deleteJob(jobName);
-	}
 
 }
